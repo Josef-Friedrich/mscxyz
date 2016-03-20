9c4d8462b4dada4d5dd69d6133c6a8c19c05f466
Josef Friedrich <josef@friedrich.rocks>
2016-03-20 08:57:32 +0100

    Auto commit (vaio)

--- bin/MuseScore_move-lyrics.py
+++ bin/MuseScore_move-lyrics.py
@@ -1,6 +1,14 @@
 #! /usr/bin/env python
 
 import lxml.etree as et
+import sys
+
+if len(sys.argv) < 3:
+    print('Usage: ' + sys.argv[0] + 'MuseScoreFile.mscx <lyrics-number>')
+    sys.exit()
+else:
+    print('troll')
+
 mscx = et.parse('affen.mscx')
 
 for lyric in mscx.findall('.//Lyrics'):
@@ -11,8 +19,9 @@ for lyric in mscx.findall('.//Lyrics'):
         no = '0'
 
     if no != '2':
-        print('nicht zwei')
         lyric.getparent().remove(lyric)
+    else:
+        number.text = '0'
 
 # To get closing tag use method 'html'
 mscx.write('affen_out.mscx', pretty_print=True, xml_declaration=True, method='html', encoding='UTF-8')
