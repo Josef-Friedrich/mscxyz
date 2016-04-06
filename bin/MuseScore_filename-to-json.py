4cf95f4c6ab738e1004fc11d656a38dec2ac26c7
Josef Friedrich <josef@friedrich.rocks>
2016-04-06 12:52:49 +0200

    Auto commit (vaio)

--- bin/MuseScore_filename-to-json.py
+++ bin/MuseScore_filename-to-json.py
@@ -8,19 +8,19 @@ if len(sys.argv) < 2:
     print('Usage: ' + sys.argv[0] + ' <musescore-file.mscx>')
     sys.exit()
 
-
+ms_path = os.path.abspath(sys.argv[1])
 ms_file = os.path.basename(sys.argv[1])
 
-ms_file = ms_file.replace('.mscx', '')
 
-print(ms_file)
+title = ms_file.replace('.mscx', '')
 
-sys.exit()
-
+cleaned_path = ms_path.replace(' ', '-')
 
 data = {}
-data['title'] = ms_file
+data['title'] = title
 
-out_file = open(ms_file + '.json','w')
+out_file = open(cleaned_path.replace('.mscx', '.json'), 'w')
 json.dump(data, out_file, indent=4)                                    
 out_file.close()
+
+os.rename(ms_path, cleaned_path)
