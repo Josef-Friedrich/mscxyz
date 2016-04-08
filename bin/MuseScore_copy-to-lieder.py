c7088a1bacaec86dcea280c6d2daa3fc35df4b8c
Josef Friedrich <josef@friedrich.rocks>
2016-04-08 21:36:41 +0200

    Auto commit (vaio)

--- bin/MuseScore_copy-to-lieder.py
+++ bin/MuseScore_copy-to-lieder.py
@@ -2,25 +2,15 @@
 
 import sys
 import os
-import json
-import errno
 import shutil
+import musescore
 
-sys.exit()
-
-def create_dir(path):
-    try:
-        os.makedirs(path)
-    except OSError as exception:
-        if exception.errno != errno.EEXIST:
-            raise
 if len(sys.argv) < 2:
     print('Usage: ' + sys.argv[0] + ' <musescore-file.mscx>')
     sys.exit()
 
 ms_path = os.path.abspath(sys.argv[1])
 ms_file = os.path.basename(sys.argv[1])
-
 
 title = ms_file.replace('.mscx', '')
 
@@ -32,17 +22,13 @@ data = {}
 data['title'] = title
 
 json_file = cleaned_path.replace('.mscx', '.json')
-out_file = open(json_file, 'w')
-json.dump(data, out_file, indent=4)
-out_file.close()
+musescore.create_info(json_file, data)
 
 os.rename(ms_path, cleaned_path)
 
-home = os.path.expanduser('~')
-
-lieder_folder = home + '/git-repositories/content/lieder/songs' + '/' + folder
-create_dir(lieder_folder)
+lieder_folder = musescore.get_lieder_folder()
+lieder_folder = lieder_folder + folder
+musescore.create_dir(lieder_folder)
 
 shutil.copy2(cleaned_path, lieder_folder + '/score.mscx')
 shutil.copy2(json_file, lieder_folder + '/info.json')
-
--- bin/musescore.py
+++ bin/musescore.py
@@ -1,5 +1,8 @@
 import os
+import sys
 import subprocess
+import json
+import errno
 
 def get_style_folder():
 	style_folder = 'MuseScore2/Stile'
@@ -15,3 +18,18 @@ def re_open(input_file, output_file):
 		subprocess.call([mac_ms, "-o", output_file, input_file])
 	else:
 		subprocess.call(["mscore", "-o", output_file, input_file])
+
+def create_info(json_file, data):
+	out_file = open(json_file, 'w')
+	json.dump(data, out_file, indent=4)
+	out_file.close()
+
+def create_dir(path):
+	try:
+		os.makedirs(path)
+	except OSError as exception:
+		if exception.errno != errno.EEXIST:
+			raise
+
+def get_lieder_folder():
+	return os.path.expanduser('~') + '/git-repositories/content/lieder/songs/'
