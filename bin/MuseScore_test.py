8479f59e860e824db3dc8ece75dd4e3da58a66ef
Josef Friedrich <josef@friedrich.rocks>
2016-04-17 20:01:12 +0200

    Auto commit (fractal)

--- bin/MuseScore_test.py
+++ bin/MuseScore_test.py
@@ -1,20 +1,8 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
 import musescore
 
-numbers = ['1', '2', '3', '4', '5']
-
-for number in numbers:
-	print(number)
-	files = musescore.get_files('mxl.' + number)
-
-	for score in files:
-		new_number = int(number) + 1 
-		new_score = score.replace('.mxl.' + number, '[' + str(new_number) + '].mxl')
-		print(score + ' -> ' + new_score)
-		os.rename(score, new_score)
-
-		#musescore.convert_mxl(file)
-
+files = musescore.get_files('mxl')
+for score in files:
+	musescore.convert_mxl(score)
--- bin/musescore.py
+++ bin/musescore.py
@@ -80,13 +80,11 @@ def rename_bad_musicxml_extensions():
 	numbers = ['1', '2', '3', '4', '5']
 
 	for number in numbers:
-		print(number)
 		files = musescore.get_files('mxl.' + number)
 
 		for score in files:
 			new_number = int(number) + 1 
 			new_score = score.replace('.mxl.' + number, '[' + str(new_number) + '].mxl')
-			print(score + ' -> ' + new_score)
 			os.rename(score, new_score)
 
 class Rename:
