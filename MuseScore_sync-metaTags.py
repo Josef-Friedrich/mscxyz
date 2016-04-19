568b5946bf3fc41e31a437c9ffe4146e70e93c1e
Josef Friedrich <josef@friedrich.rocks>
2016-04-19 18:15:16 +0200

    Add library for argument parsing

--- MuseScore_sync-metaTags.py
+++ MuseScore_sync-metaTags.py
@@ -1,9 +1,17 @@
 #! /usr/bin/env python
 
+import argparse
 import musescore
 
-musescore.catch_args()
+parser = argparse.ArgumentParser(description='Synchronize the values of\
+	the first vertical frame (title, composer, lyricist) with the \
+	corresponding metadata fields.')
+parser.add_argument('path', help='Path to a *.mscx file or a folder\
+	which contains *.mscx files.')
+parser.add_argument('-j', '--json', action='store_true',
+	help='Additionally write the metadata to a json file.')
+args = parser.parse_args()
 
-files = musescore.get_mscx(musescore.score)
+files = musescore.get_mscx(args.path)
 for score in files:
 	print(score)
