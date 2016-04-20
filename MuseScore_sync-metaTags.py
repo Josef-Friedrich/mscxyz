64f264950b18157cc6a1468954c667e2e9973fa1
Josef Friedrich <josef@friedrich.rocks>
2016-04-20 10:38:48 +0200

    Add subcommand

--- MuseScore_sync-metaTags.py
+++ MuseScore_sync-metaTags.py
@@ -10,6 +10,12 @@ parser.add_argument('path', help='Path to a *.mscx file or a folder\
 	which contains *.mscx files.')
 parser.add_argument('-j', '--json', action='store_true',
 	help='Additionally write the metadata to a json file.')
+
+
+subparsers = parser.add_subparsers(help='Extract lyrics')
+parser_lyrics = subparsers.add_parser('lyrics', help='Extract lyrics')
+parser_lyrics.add_argument('-n', '--number', action='store_true', help='Number of lyrics verse.')
+
 args = parser.parse_args()
 
 files = musescore.get_mscx(args.path)
