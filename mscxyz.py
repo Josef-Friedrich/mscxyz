1715c41a03d81f16bc3b041ce413c690fc0e14d0
Josef Friedrich <josef@friedrich.rocks>
2016-04-26 17:26:38 +0200

    Implement exit gracefully

--- mscxyz.py
+++ mscxyz.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+import signal
 import lxml.etree
 from termcolor import colored, cprint
 
@@ -247,6 +248,24 @@ def create_dir(path):
 	except OSError as exception:
 		if exception.errno != errno.EEXIST:
 			raise
+
+def exit_gracefully(signum, frame):
+	# Restore the original signal handler as otherwise evil things will
+	# happen, in raw_input when CTRL+C is pressed, and our signal
+	# handler is not re-entrant
+	signal.signal(signal.SIGINT, original_sigint)
+
+	try:
+		if raw_input("\nReally quit? (y/n)> ").lower().startswith('y'):
+			print('Quitting ...')
+			sys.exit(1)
+
+	except KeyboardInterrupt:
+		print("Ok ok, quitting")
+		sys.exit(1)
+
+	# restore the exit gracefully handler here
+	signal.signal(signal.SIGINT, exit_gracefully)
 
 def print_desc(text, description='', color='red'):
 	prefix = ''
@@ -537,7 +556,10 @@ class Meta(Tree):
 					print_desc(text, tag, 'green')
 
 if __name__ == '__main__':
+
+	original_sigint = signal.getsignal(signal.SIGINT)
+	signal.signal(signal.SIGINT, exit_gracefully)
+
 	parse = Parse()
 	args = parse.parse()
 	execute()
-
