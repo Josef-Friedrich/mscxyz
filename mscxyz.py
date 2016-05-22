e639be4149cb96d155aaa42c7b32398557049461
Josef Friedrich <josef@friedrich.rocks>
2016-05-22 13:18:01 +0200

    Shift all functions at the beginning of the file

--- mscxyz.py
+++ mscxyz.py
@@ -10,6 +10,66 @@ from termcolor import colored, cprint
 reload(sys)
 sys.setdefaultencoding('utf8')
 
+def mscore(commands):
+	import subprocess
+	mac = '/Applications/MuseScore.app/Contents/MacOS/mscore'
+	if os.path.exists(mac):
+		executeable = mac
+	else:
+		executeable = 'mscore'
+
+	commands.insert(0, executeable)
+	if args.verbose > 2:
+		OUT=None
+	else:
+		OUT = open(os.devnull, 'wb')
+
+	subprocess.call(commands, stdout=OUT, stderr=OUT)
+
+def re_open(input_file):
+	mscore(['-o', input_file, input_file])
+
+def convert_mxl(input_file):
+	output_file = input_file.replace('.mxl', '.mscx')
+	mscore(['-o', output_file, input_file])
+	os.remove(input_file)
+
+def create_dir(path):
+	import errno
+	try:
+		os.makedirs(path)
+	except OSError as exception:
+		if exception.errno != errno.EEXIST:
+			raise
+
+def exit_gracefully(signum, frame):
+	# Restore the original signal handler as otherwise evil things will
+	# happen, in raw_input when CTRL+C is pressed, and our signal
+	# handler is not re-entrant
+	signal.signal(signal.SIGINT, original_sigint)
+
+	try:
+		if raw_input("\nReally quit? (y/n)> ").lower().startswith('y'):
+			print('Quitting ...')
+			sys.exit(1)
+
+	except KeyboardInterrupt:
+		print("Ok ok, quitting")
+		sys.exit(1)
+
+	# restore the exit gracefully handler here
+	signal.signal(signal.SIGINT, exit_gracefully)
+
+def print_desc(text, description='', color='red'):
+	prefix = ''
+	if description:
+		prefix = colored(description, color) + ': '
+	print(prefix + text)
+
+def verbose(text, description='', color='red', verbosity=1):
+	if args.verbose >= verbosity:
+		print_desc(text=text, description=description, color=color)
+
 class Parse(object):
 	"""Expose the command line interface."""
 
@@ -228,66 +288,6 @@ class Batch(object):
 			return self.files
 		else:
 			return [self.path]
-
-def mscore(commands):
-	import subprocess
-	mac = '/Applications/MuseScore.app/Contents/MacOS/mscore'
-	if os.path.exists(mac):
-		executeable = mac
-	else:
-		executeable = 'mscore'
-
-	commands.insert(0, executeable)
-	if args.verbose > 2:
-		OUT=None
-	else:
-		OUT = open(os.devnull, 'wb')
-
-	subprocess.call(commands, stdout=OUT, stderr=OUT)
-
-def re_open(input_file):
-	mscore(['-o', input_file, input_file])
-
-def convert_mxl(input_file):
-	output_file = input_file.replace('.mxl', '.mscx')
-	mscore(['-o', output_file, input_file])
-	os.remove(input_file)
-
-def create_dir(path):
-	import errno
-	try:
-		os.makedirs(path)
-	except OSError as exception:
-		if exception.errno != errno.EEXIST:
-			raise
-
-def exit_gracefully(signum, frame):
-	# Restore the original signal handler as otherwise evil things will
-	# happen, in raw_input when CTRL+C is pressed, and our signal
-	# handler is not re-entrant
-	signal.signal(signal.SIGINT, original_sigint)
-
-	try:
-		if raw_input("\nReally quit? (y/n)> ").lower().startswith('y'):
-			print('Quitting ...')
-			sys.exit(1)
-
-	except KeyboardInterrupt:
-		print("Ok ok, quitting")
-		sys.exit(1)
-
-	# restore the exit gracefully handler here
-	signal.signal(signal.SIGINT, exit_gracefully)
-
-def print_desc(text, description='', color='red'):
-	prefix = ''
-	if description:
-		prefix = colored(description, color) + ': '
-	print(prefix + text)
-
-def verbose(text, description='', color='red', verbosity=1):
-	if args.verbose >= verbosity:
-		print_desc(text=text, description=description, color=color)
 
 class File(object):
 	def __init__(self, fullpath):
