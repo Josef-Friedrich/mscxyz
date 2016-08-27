666634995a1711beeb828a8f18cfd1ed05bb2ffb
Josef Friedrich <josef@friedrich.rocks>
2016-08-27 08:29:17 +0200

    Convert spaces to tabs

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup
 
 setup(
-    name = 'mscxyz',
-    version = '0.0.2',
-    author = 'Josef Friedrich',
-    author_email = 'josef@friedrich.rocks',
-    description = ('A command line tool to manipulate the XML based *.mscX and *.mscZ files of the notation software MuseScore.'),
-    license = 'MIT',
-    packages = ['mscxyz'],
-    keywords = 'audio',
-    url = 'https://github.com/Josef-Friedrich/mscxyz',
-    install_requires = [
-        'lxml',
-    ],
-    classifiers = [
-        'Development Status :: 3 - Alpha',
-    ],
-    zip_safe=False,
+	name = 'mscxyz',
+	version = '0.0.2',
+	author = 'Josef Friedrich',
+	author_email = 'josef@friedrich.rocks',
+	description = ('A command line tool to manipulate the XML based *.mscX and *.mscZ files of the notation software MuseScore.'),
+	license = 'MIT',
+	packages = ['mscxyz'],
+	keywords = 'audio',
+	url = 'https://github.com/Josef-Friedrich/mscxyz',
+	install_requires = [
+		'lxml',
+	],
+	classifiers = [
+		'Development Status :: 3 - Alpha',
+	],
+	zip_safe=False,
 )
--- test/test.py
+++ test/test.py
@@ -4,15 +4,15 @@ import unittest
 import mscxyz.meta as m
 
 def get_testfile(filename):
-    return os.path.join(os.path.dirname(__file__), './' + filename)
+	return os.path.join(os.path.dirname(__file__), './' + filename)
 
 meta = m.Meta(get_testfile('test.mscx'))
 
 class TestMeta(unittest.TestCase):
 
-    def test_get(self):
-        self.assertEqual(meta.get('title'), 'Title')
-        self.assertEqual(meta.get('composer'), 'Composer')
+	def test_get(self):
+		self.assertEqual(meta.get('title'), 'Title')
+		self.assertEqual(meta.get('composer'), 'Composer')
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
