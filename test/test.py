7ef05dcbc7034fcaac28c93518b43447b0d71829
Josef Friedrich <josef@friedrich.rocks>
2016-08-29 18:14:11 +0200

    Improve tests

--- test/test.py
+++ test/test.py
@@ -1,18 +1,30 @@
 import os
 import unittest
 
-import mscxyz.meta as m
-
 def get_testfile(filename):
 	return os.path.join(os.path.dirname(__file__), './' + filename)
 
-meta = m.Meta(get_testfile('test.mscx'))
-
 class TestMeta(unittest.TestCase):
+	def setUp(self):
+		from mscxyz.meta import Meta
+		self.meta = Meta(get_testfile('test.mscx'), args=None)
 
 	def test_get(self):
-		self.assertEqual(meta.get('title'), 'Title')
-		self.assertEqual(meta.get('composer'), 'Composer')
+		self.assertEqual(self.meta.get('title'), 'Title')
+		self.assertEqual(self.meta.get('composer'), 'Composer')
+
+class TestFile(unittest.TestCase):
+
+	def setUp(self):
+		from mscxyz.fileloader import File
+		self.file = File(get_testfile('test.mscx'), args=None)
+
+	def test_file_object_initialisation(self):
+		self.assertEqual(self.file.fullpath, get_testfile('test.mscx'))
+		self.assertEqual(self.file.dirname, os.path.dirname(get_testfile('test.mscx')))
+		self.assertEqual(self.file.filename, 'test.mscx')
+		self.assertEqual(self.file.basename, 'test')
+		self.assertEqual(self.file.extension, 'mscx')
 
 if __name__ == '__main__':
 	unittest.main()
