80a61df8dcf154fdae76d7499045a01f8d6d630d
Josef Friedrich <josef@friedrich.rocks>
2016-09-01 09:12:19 +0200

    Add tests git push

--- test/test.py
+++ test/test.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+import mscxyz
 
 def get_testfile(filename):
 	return os.path.join(os.path.dirname(__file__), './' + filename)
@@ -25,6 +26,26 @@ class TestFile(unittest.TestCase):
 		self.assertEqual(self.file.filename, 'test.mscx')
 		self.assertEqual(self.file.basename, 'test')
 		self.assertEqual(self.file.extension, 'mscx')
+
+class TestCommandlineInterface(unittest.TestCase):
+
+	def test_help_short(self):
+		with self.assertRaises(SystemExit) as cm:
+			mscxyz.execute(['-h'])
+		the_exception = cm.exception
+		self.assertEqual(str(the_exception), '0')
+
+	def test_help_long(self):
+		with self.assertRaises(SystemExit) as cm:
+			mscxyz.execute(['--help'])
+		the_exception = cm.exception
+		self.assertEqual(str(the_exception), '0')
+
+	def test_without_arguments(self):
+		with self.assertRaises(SystemExit) as cm:
+			mscxyz.execute()
+		the_exception = cm.exception
+		self.assertEqual(str(the_exception), '2')
 
 if __name__ == '__main__':
 	unittest.main()
