477d9990db831c35c741262ecef238a346b08d42
Josef Friedrich <josef@friedrich.rocks>
2016-09-03 20:14:05 +0200

    Add LICENSE

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
 	name = 'mscxyz',
-	version = '0.0.2',
+	version = '0.0.3',
 	author = 'Josef Friedrich',
 	author_email = 'josef@friedrich.rocks',
 	description = ('A command line tool to manipulate the XML based *.mscX and *.mscZ files of the notation software MuseScore.'),
