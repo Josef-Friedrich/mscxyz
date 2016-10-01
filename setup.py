c07eb988470a0cb4577fad5da7aa5d176c903f8e
Josef Friedrich <josef@friedrich.rocks>
2016-10-02 00:04:19 +0200

    Version 0.0.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name='mscxyz',
-    version='0.0.4',
+    version='0.0.5',
     author='Josef Friedrich',
     author_email='josef@friedrich.rocks',
     description=(
