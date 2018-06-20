54f777b2545f09b011874b476ad92bffaab2602f
Josef Friedrich <josef@friedrich.rocks>
2018-06-20 17:36:17 +0200

    Add autodoc_default_flags

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,4 +1,6 @@
 import sphinx_rtd_theme
+import mscxyz
+
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
@@ -14,34 +16,12 @@ master_doc = 'index'
 project = u'mscxyz'
 copyright = u'2016, Josef Friedrich'
 author = u'Josef Friedrich'
-version = u'0.0.5'
-release = u'0.0.5'
+version = mscxyz.__version__
+release = mscxyz.__version__
 language = None
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 pygments_style = 'sphinx'
 todo_include_todos = False
 html_static_path = []
 htmlhelp_basename = 'mscxyzdoc'
-
-latex_elements = {
-     'papersize': 'a4paper',
-     'pointsize': '11pt',
-}
-
-latex_documents = [
-    (master_doc, 'mscxyz.tex', u'mscxyz Documentation',
-     u'Josef Friedrich', 'manual'),
-]
-
-man_pages = [
-    (master_doc, 'mscxyz', u'mscxyz Documentation',
-     [author], 1)
-]
-
-texinfo_documents = [
-    (master_doc, 'mscxyz', u'mscxyz Documentation',
-     author, 'mscxyz',
-     u'Manipulate the XML based *.mscx files of the notation software \
-     MuseScore.',
-     'Miscellaneous'),
-]
+autodoc_default_flags = ['members', 'undoc-members', 'private-members', 'show-inheritance']
--- doc/source/mscxyz.rst
+++ doc/source/mscxyz.rst
@@ -5,9 +5,6 @@ Module contents
 ---------------
 
 .. automodule:: mscxyz
-    :members:
-    :undoc-members:
-    :show-inheritance:
 
 Submodules
 ----------
@@ -16,46 +13,28 @@ mscxyz.score_file_classes module
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 .. automodule:: mscxyz.score_file_classes
-    :members:
-    :undoc-members:
-    :show-inheritance:
 
 mscxyz.lyrics module
 ^^^^^^^^^^^^^^^^^^^^
 
 .. automodule:: mscxyz.lyrics
-    :members:
-    :undoc-members:
-    :show-inheritance:
 
 mscxyz.meta module
 ^^^^^^^^^^^^^^^^^^
 
 .. automodule:: mscxyz.meta
-    :members:
-    :undoc-members:
-    :show-inheritance:
 
 mscxyz.cli module
 ^^^^^^^^^^^^^^^^^
 
 .. automodule:: mscxyz.cli
-    :members:
-    :undoc-members:
-    :show-inheritance:
 
 mscxyz.rename module
 ^^^^^^^^^^^^^^^^^^^^
 
 .. automodule:: mscxyz.rename
-    :members:
-    :undoc-members:
-    :show-inheritance:
 
 mscxyz.utils module
 ^^^^^^^^^^^^^^^^^^^
 
 .. automodule:: mscxyz.utils
-    :members:
-    :undoc-members:
-    :show-inheritance:
