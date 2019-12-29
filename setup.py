9256f7dbd477eec4ba288c6e590ade0e70800864
Josef Friedrich <josef@friedrich.rocks>
2019-12-29 23:28:36 +0100

    Support py38

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     keywords='audio',
     url='https://github.com/Josef-Friedrich/mscxyz',
     install_requires=[
-        'lxml', 'termcolor', 'tmep>=1.1.2'
+        'lxml', 'termcolor', 'tmep>=2.0.1'
     ],
     tests_require=['mock'],
     scripts=['bin/mscx-manager'],
@@ -42,6 +42,8 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3',
     ],
     zip_safe=False,
