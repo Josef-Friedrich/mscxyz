90e4bc8fa793ac8809dc204b3f2561d975e4737d
Josef Friedrich <josef@friedrich.rocks>
2024-01-15 17:03:14 +0100

    Fix doc warnings

--- mscxyz/__init__.py
+++ mscxyz/__init__.py
@@ -18,16 +18,12 @@ __version__: str = metadata.version("mscxyz")
 supported_versions = (2, 3, 4)
 
 Score = mscxyz.score.Score
-"""Score""" ""
 
 Lyrics = mscxyz.lyrics.Lyrics
-"""Lyrics"""
 
 Meta = mscxyz.meta.Meta
-"""Meta"""
 
 Style = mscxyz.style.Style
-"""Style"""
 
 list_score_paths = utils.list_score_paths
 """list_score_paths"""
