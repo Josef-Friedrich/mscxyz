8551cdd43bbda35c25247359624d32236736d7c9
Josef Friedrich <josef@friedrich.rocks>
2024-01-26 17:38:02 +0100

    Test the option --bail

--- tests/test_command_line_interface.py
+++ tests/test_command_line_interface.py
@@ -1,9 +1,11 @@
 """Test the command line interface using subprocess."""
+
 from __future__ import annotations
 
 import os
 
 from mscxyz import Score
+from tests import helper
 from tests.helper import Cli
 
 
@@ -19,3 +21,13 @@ class TestOptionBackup:
     def test_size(self) -> None:
         score: Score = Cli("--backup", "--dry-run").score()
         assert os.path.getsize(score.path) == os.path.getsize(score.backup_file)
+
+
+class TestOptionBail:
+    def test_no_bail(self) -> None:
+        assert "XMLSyntaxError" in Cli(helper.get_file("broken.mscx")).stdout()
+
+    def test_bail(self) -> None:
+        assert (
+            "XMLSyntaxError" in Cli("--bail", helper.get_file("broken.mscx")).sysexit()
+        )
